020d47525440895d72ab64af042bd921ad7663bd
Arditya B <arditbaskara2@gmail.com>
2025-03-12 11:21:17 +0700

    debug

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,4 +1,5 @@
-import User from "../models/user.model";
+import User from '../models/user.model';
+
 import { connect } from '../mongodb/mongoose';
 
 export const createOrUpdateUser = async (
@@ -7,32 +8,34 @@ export const createOrUpdateUser = async (
   last_name,
   image_url,
   email_addresses,
-  username) => {
-    try {
-      await connect();
-      const user = await User.findOneAndUpdate(
-        { clerkId: id },
-        {
-          $set: {
-            firstName: first_name,
-            lastName: last_name,
-            profilePicture: image_url,
-            email: email_addresses[0].email_address,
-            username,
-          },
-    }, {new: true, upset: true} 
-  );
+  username
+) => {
+  try {
+    await connect();
+    const user = await User.findOneAndUpdate(
+      { clerkId: id },
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          profilePicture: image_url,
+          email: email_addresses[0].email_address,
+          username,
+        },
+      },
+      { new: true, upsert: true }
+    );
     return user;
   } catch (error) {
-      throw new Error('Failed to create or update user: ', error);
-    }
+    console.log('Error creating or updating user:', error);
   }
+};
 
-  export const deleteUser = async (id) => {
-    try{
-      await connect();
-      const user = await User.findOneAndDelete({ clerkId: id });
-    } catch (error) {
-      throw new Error('Failed to delete user: ', error);
-    }
-  };
+export const deleteUser = async (id) => {
+  try {
+    await connect();
+    await User.findOneAndDelete({ clerkId: id });
+  } catch (error) {
+    console.log('Error deleting user:', error);
+  }
+};
